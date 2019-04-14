e854ac9e8a6b1caf8ef0a544e9172f4d0a921c02
Stephen Lin <stephen.lin@titansoft.com.sg>
2019-04-14 22:51:00 +0800

    Merge StringAverage and ProductOfArrayItems into one .sln

---
+++ Kata/ArrayMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata
+{
+    public class ArrayMath
+    {
+        public static int Product(int[] values)
+        {
+            if (values == null)
+                throw new ArgumentNullException();
+            if (values.Length == 0)
+                throw new InvalidOperationException();
+
+            var result = 1;
+            foreach (var item in values)
+            {
+                result *= item;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Kata/StringAverage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace Kata
+{
+    public class StringAverage
+    {
+        public static string AverageString(string input)
+        {
+            var error = "n/a";
+
+            if (input == string.Empty)
+                return error;
+            if (input.Split(" ").Length == 0)
+                return error;
+
+            try
+            {
+                var average = input.Split(" ")
+                    .ToList()
+                    .Select(x => x.TextToInt())
+                    .Average();
+                return ((int) Math.Floor(average)).IntToText();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return error;
+            }
+        }
+    }
+
+    public static class Extensions
+    {
+        private static readonly string[] Words = new string[]
+        {
+            "zero", "one", "two", "three", "four", "five",
+            "six", "seven", "eight", "nine"
+        };
+
+        public static int TextToInt(this string input)
+        {
+            if(Words.Contains(input)!=true)
+                throw new ArgumentOutOfRangeException();
+            return Array.IndexOf(Words, input);
+        }
+
+
+        public static string IntToText(this int input)
+        {
+            if (input < 0 || input > Words.Length - 1)
+                throw new AbandonedMutexException();
+            return Words[input];
+        }
+    }
+}
