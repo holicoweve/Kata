e07cc76e57caeeac4e790b476bfa1e81a0092f11
Stephen Lin <stephen.lin@titansoft.com.sg>
2019-04-21 22:02:05 +0800

    work in progress ThreeSums

---
+++ KataTests/ThreeSumKataTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Kata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataTests
+{
+    [TestClass]
+    public class ThreeSumKataTest
+    {
+        [TestMethod]
+        public void SimpleTest()
+        {
+            // TODO: need to figure out how to test equality of nested arrays
+            
+            ThreeSumKata kata = new ThreeSumKata();
+
+            var expected = new List<IList<int>> { new List<int> { -1, 0, 1 }, new List<int>() { -1, -1, 2 } };
+            var actual = kata.ThreeSum(new int[] { -1, 0, 1, 2, -1, -4 });
+            //CollectionAssert.AreEquivalent(expected, actual as List<IList<int>>);
+        }
+    }
+}
