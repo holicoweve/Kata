b8f2447e647b38472f8572eb41979d6c7ccaa7f3
Stephen Lin <stephen.lin@titansoft.com.sg>
2019-05-12 00:29:13 +0800

    finished SumStringsAsNumbers

---
+++ KataTests/SumStringsAsNumbersTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataTests
+{
+    [TestClass]
+    public class SumStringsAsNumbersTest
+    {
+        private SumStringsAsNumbers _kata;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _kata = new SumStringsAsNumbers();
+        }
+
+        [TestMethod]
+        public void SingleDigit()
+        {
+            Assert.AreEqual("3",_kata.Sum("1","2"));
+        }
+
+        [TestMethod]
+        public void NoCarryOver()
+        {
+            Assert.AreEqual("468", _kata.Sum("123", "345"));
+        }
+
+        [TestMethod]
+        public void CarryOver()
+        {
+            Assert.AreEqual("1998",_kata.Sum("999","999"));
+        }
+
+        [TestMethod]
+        public void LengthPlusOne()
+        {
+            Assert.AreEqual("1033",_kata.Sum("511","522"));
+        }
+
+        [TestMethod]
+        public void UnequalLength()
+        {
+            Assert.AreEqual("12346",_kata.Sum("1","12345"));
+        }
+
+    }
+}
