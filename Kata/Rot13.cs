e5a07c7df22c2b4c508f312843f48669f5301064
Stephen Lin <stephen.lin@titansoft.com.sg>
2019-06-02 00:21:38 +0800

    finished Rot13 and test

---
+++ Kata/Rot13.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata
+{
+    public class Rot13
+    {
+        public string Encode(string input)
+        {
+            var builder = new StringBuilder();
+            foreach (var c in input.AsEnumerable())
+            {
+                builder.Append(Transform(c));
+            }
+            return builder.ToString();
+        }
+
+        private static char Transform(char input)
+        {
+            if (input >= 'a' && input <= 'z')
+            {
+                return (char)('a' + (input - 'a' + 13) % 26);
+            }
+
+            if (input >= 'A' && input <= 'Z')
+            {
+                return (char)('A' + (input - 'A' + 13) % 26);
+            }
+
+            return input;
+        }
+    }
+}
---
+++ KataTests/Rot13Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kata;
+
+namespace KataTests
+{
+    [TestClass]
+    public class Rot13Test
+    {
+        private Rot13 _kata;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _kata = new Rot13();
+        }
+
+        [TestMethod]
+        public void UpperCaseOnly()
+        {
+            Assert.AreEqual("TEST", _kata.Encode("GRFG"));
+        }
+
+        [TestMethod]
+        public void LowerCaseOnly()
+        {
+            Assert.AreEqual("test", _kata.Encode("grfg"));
+        }
+
+        [TestMethod]
+        public void NumbersShouldBeUnaffected()
+        {
+            Assert.AreEqual("123", _kata.Encode("123"));
+        }
+
+        [TestMethod]
+        public void SpaceShouldBeUnaffected()
+        {
+            Assert.AreEqual(" ", _kata.Encode(" "));
+        }
+    }
+}
