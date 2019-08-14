d190505ced9a3cfc1bf8fe13cee066c50de4df69
Stephen Lin <stephen.lin@titansoft.com.sg>
2019-08-14 22:09:20 +0800

    add tests for StringToInteger

---
+++ Kata/StringToInteger_atoi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata
+{
+	public class StringToInteger_atoi
+	{
+		public int Atoi(string input) 
+		{
+			return 0;
+		}
+	}
+}
---
+++ KataTests/StringToIntegerTest.cs
@@ -0,0 +1,84 @@
+﻿using Kata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace KataTests
+{
+	[TestClass]
+	public class StringToIntegerTest
+	{
+		private StringToInteger_atoi _kata;
+
+		[TestInitialize]
+		public void Init()
+		{
+			_kata = new StringToInteger_atoi();
+		}
+
+		[TestMethod]
+		public void PositiveInteger()
+		{
+			var input = "42";
+
+			var expected = 42;
+			var actual = _kata.Atoi(input);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void NegativeInteger()
+		{
+			var input = "-42";
+
+			var expected = -42;
+			var actual = _kata.Atoi(input);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void LeadingWhiteSpace()
+		{
+			var input = "      -42";
+
+			var expected = -42;
+			var actual = _kata.Atoi(input);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void TrailingWords()
+		{
+			var input = "4193 with words";
+
+			var expected = 4193;
+			var actual = _kata.Atoi(input);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void LeadingWords()
+		{
+			var input = "words and 987";
+
+			var expected = 0;
+			var actual = _kata.Atoi(input);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void LessThanIntMin()
+		{
+			var input = "-91283472332";
+
+			var expected = -2147483648;
+			var actual = _kata.Atoi(input);
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
