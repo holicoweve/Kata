1cb1764080f50108d8ac75c25ace941849ad4fbc
Stephen Lin <stephen.lin@titansoft.com.sg>
2019-08-16 09:36:35 +0800

    added tests for CountAndSay

---
+++ Kata/CountAndSay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Kata
+{
+	public class CountAndSay_Sln
+	{
+		public string CountAndSay(int n)
+		{
+			return string.Empty;
+		}
+
+		public string Say(string input)
+		{
+			var groupBy = input.GroupBy(c => c);
+
+			return string.Empty;
+		}
+	}
+}
---
+++ KataTests/CountAndSaySlnTest.cs
@@ -0,0 +1,30 @@
+﻿using Kata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataTests
+{
+	[TestClass]
+	public class CountAndSaySlnTest
+	{
+		private CountAndSay_Sln _kata;
+
+		[TestInitialize]
+		public void Init()
+		{
+			_kata = new CountAndSay_Sln();
+		}
+
+		[DataTestMethod]
+		[DataRow(1, "1")]
+		[DataRow(2, "11")]
+		[DataRow(3, "21")]
+		[DataRow(4, "1211")]
+		[DataRow(5, "111221")]
+		public void CountAndSayTest(int n, string expected)
+		{
+			var actual = _kata.CountAndSay(n);
+			Assert.AreEqual(expected, actual);
+		}
+		
+	}
+}
