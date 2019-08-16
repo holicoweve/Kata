7d6e24dd255456309b8f4493c957298dc41b2579
Stephen Lin <stephen.lin@titansoft.com.sg>
2019-08-16 23:45:41 +0800

    completed LongestCommonPrefix

---
+++ Kata/LongestCommonPrefix.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata
+{
+	/// <summary>
+	/// Longest Common Prefix
+	/// https://leetcode.com/problems/longest-common-prefix/
+	/// </summary>
+	public class LongestCommonPrefix
+	{
+		public string FindLongestCommonPrefix(string[] inputs)
+		{
+			var prefix = string.Empty;
+			
+			if (inputs.Length == 0)
+				return prefix;
+
+			var shortestItem = inputs.OrderBy(i => i.Length).First();
+			var maxLength = shortestItem.Length;
+
+			for(var i = 1;i<=maxLength;i++)
+			{
+				var prevPrefix = prefix;
+				prefix = shortestItem.Substring(0, i);
+				foreach (var item in inputs)
+				{
+					if (!item.StartsWith(prefix))
+						return prevPrefix;
+				}
+			}
+
+			return shortestItem;
+		}
+
+		private string CommonPrefix(string input1, string input2)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ KataTests/LongestCommonPrefixTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataTests
+{
+	[TestClass]
+	public class LongestCommonPrefixTest
+	{
+		private LongestCommonPrefix _kata;
+
+		[TestInitialize]
+		public void Init()
+		{
+			_kata = new LongestCommonPrefix();
+		}
+
+		[TestMethod]
+		public void OneItem()
+		{
+			var input = new[] { "flower" };
+			var expected = "flower";
+			var actual = _kata.FindLongestCommonPrefix(input);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void OneCharLength()
+		{
+			var input = new[] { "a","b" };
+			var expected = "";
+			var actual = _kata.FindLongestCommonPrefix(input);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void RepeatingItems()
+		{
+			var input = new[] { "flowerflower","flower" };
+			var expected = "flower";
+			var actual = _kata.FindLongestCommonPrefix(input);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void SimpleTest()
+		{
+			var input = new[] { "flower", "flow", "flight" };
+			var expected = "fl";
+			var actual = _kata.FindLongestCommonPrefix(input);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void NoSolution()
+		{
+			var input = new[] { "dog", "racecar", "car" };
+			var expected = "";
+			var actual = _kata.FindLongestCommonPrefix(input);
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
