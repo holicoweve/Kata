1df646a03924300ed3a2aa66dacc142aee2730cd
Stephen Lin <stephen.lin@titansoft.com.sg>
2019-08-17 22:30:20 +0800

    added tests for Longest Substring Without Repeating Characters

---
+++ Kata/LongestSubstringWithoutRepeat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Kata
+{
+	/// <summary>
+	/// Longest Substring Without Repeating Characters
+	/// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+	/// </summary>
+	public class LongestSubstringWithoutRepeat
+	{
+		private string LongestSubstring(string input)
+		{
+			return string.Empty;
+		}
+
+		public int Length(string input)
+		{
+			return LongestSubstring(input).Length;
+		}
+	}
+
+
+}
---
+++ KataTests/LongestSubstringWithoutRepeatTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataTests
+{
+	[TestClass]
+	public class LongestSubstringWithoutRepeatTest
+	{
+		private LongestSubstringWithoutRepeat _kata;
+
+		[TestInitialize]
+		public void Init()
+		{
+			_kata = new LongestSubstringWithoutRepeat();
+		}
+
+		[TestMethod]
+		public void SameChars()
+		{
+			var input = "bbbbb";
+			var expected = 1;
+			var actual = _kata.Length(input);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void AnswerInBeginning()
+		{
+			var input = "abcabcbb";
+			var expected = 3;
+			var actual = _kata.Length(input);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void AnswerInMiddle()
+		{
+			var input = "pwwkew";
+			var expected = 3;
+			var actual = _kata.Length(input);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+	}
+}
