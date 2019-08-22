bb375f3d6141d049d8563fa66f155859d801e346
Stephen <stephen.sunglun.lin@gmail.com>
2019-08-22 10:18:16 +0800

    completed RemoveNthFromEnd

--- Kata/RemoveNthFromEndKata.cs
+++ Kata/RemoveNthFromEndKata.cs
@@ -1,19 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using Kata.obj;
 
 namespace Kata
 {
 	/// <summary>
-	/// Remove Nth Node From End of List
-	/// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+	///     Remove Nth Node From End of List
+	///     https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 	/// </summary>
 	public class RemoveNthFromEndKata
 	{
 		public ListNode RemoveNthFromEnd(ListNode head, int n)
 		{
-			return new ListNode(0);
+			var nodeList = new List<ListNode>();
+			var currNode = head;
+			nodeList.Add(currNode);
+			while (currNode.Next != null)
+			{
+				currNode = currNode.Next;
+				nodeList.Add(currNode);
+			}
+
+			var skipNode = nodeList.Count - n;
+			ListNode prevNode = null;
+			head = prevNode;
+			for (var i = nodeList.Count - 1; i >= 0; i--)
+			{
+				if (i == skipNode) i--;
+				if (i < 0)
+				{
+					break;
+				}
+				currNode = nodeList[i];
+				currNode.Next = prevNode;
+
+				prevNode = currNode;
+				head = prevNode;
+			}
+
+			return head;
 		}
 	}
 }
---
+++ KataTests/RemoveNthFromEndKataTest.cs
@@ -0,0 +1,63 @@
+﻿using Castle.Core.Logging;
+using Kata;
+using Kata.obj;
+using Xunit;
+using Xunit.Sdk;
+
+namespace KataTests
+{
+	public class RemoveNthFromEndKataTest
+	{
+		public RemoveNthFromEndKataTest()
+		{
+			_kata = new RemoveNthFromEndKata();
+		}
+
+		private readonly RemoveNthFromEndKata _kata;
+
+		[Fact]
+		public void SimpleTest()
+		{
+			var inputHead =
+				new ListNode(1)
+				{
+					Next = new ListNode(2)
+					{
+						Next = new ListNode(3)
+						{
+							Next = new ListNode(4)
+							{
+								Next = new ListNode(5)
+							}
+						}
+					}
+				};
+			var n = 2;
+
+			var expected =
+				new ListNode(1)
+				{
+					Next = new ListNode(2)
+					{
+						Next = new ListNode(3)
+						{
+							Next = new ListNode(5)
+						}
+					}
+				};
+			var actual = _kata.RemoveNthFromEnd(inputHead, n);
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void OneItemTtest()
+		{
+			var inputHead = new ListNode(1);
+			var n = 1;
+
+			ListNode expected = null;
+			var actual = _kata.RemoveNthFromEnd(inputHead, n);
+			Assert.Equal(expected, actual);
+		}
+	}
+}
