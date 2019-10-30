f9c9f26b9b5a212e5b6321403bc73868ea7686b0
Stephen <stephen.sunglun.lin@gmail.com>
2019-10-30 18:01:31 +0800

    started forming magic square

---
+++ Kata/FormingMagicSquare.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata
+{
+	class FormingMagicSquare
+	{
+		private int _validArraySize = 9;
+
+		public int[,] ParseArrayToSquare(int[] input)
+		{
+			if (input.Length != _validArraySize)
+			{
+				throw new ArgumentException("Input must be 9 integers");
+			}
+
+			var result = new int[3, 3];
+			var enumerator = input.AsEnumerable().GetEnumerator();
+			enumerator.MoveNext();
+			for (var i = 0; i < 3; i++)
+			{
+				for (var j = 0; j < 3; j++)
+				{
+					result[i, j] = enumerator.Current;
+					enumerator.MoveNext();
+				}
+			}
+			enumerator.Dispose();
+
+			return result;
+		}
+
+		internal bool IsMagicSquare(int[,] inputSquare)
+		{
+			for (var i = 0; i < 3; i++)
+			{
+				if (ColumnSum(inputSquare, i) != 15)
+				{
+					return false;
+				}
+
+				if (RowSum(inputSquare, i) != 15)
+				{
+					return false;
+				}
+			}
+
+			if (DiagonalSum(inputSquare, true) != 15 || DiagonalSum(inputSquare, false) != 15)
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+		private int RowSum(int[,] inputSquare, int rowNumber)
+		{
+			if (rowNumber < 0 || rowNumber >= 3)
+			{
+				throw new ArgumentException();
+			}
+
+			var rowSum = inputSquare[rowNumber, 0] + inputSquare[rowNumber, 1] + inputSquare[rowNumber, 2];
+			return rowSum;
+		}
+
+		private int ColumnSum(int[,] inputSquare, int columnNumber)
+		{
+			if (columnNumber < 0 || columnNumber >= 3)
+			{
+				throw new ArgumentException();
+			}
+
+			var columnSum = inputSquare[0, columnNumber] + inputSquare[1, columnNumber] + inputSquare[2, columnNumber];
+			return columnSum;
+		}
+
+		private int DiagonalSum(int[,] inputSquare, bool direction)
+		{
+			var offSet = direction ? 1 : -1;
+			var diagonalSum = inputSquare[0, 1 + offSet] + inputSquare[1, 1] + inputSquare[2, 1 - offSet];
+			return diagonalSum;
+		}
+
+		internal int CalculateCost(int[,] inputSquare, int[,] targetSquare)
+		{
+			if (inputSquare.GetLength(0)!=targetSquare.GetLength(0) || inputSquare.GetLength(1)!=targetSquare.GetLength(1))
+			{
+				throw new ArgumentException("The magic squares are not the same size");
+			}
+
+			var result = 0;
+			for (var i = 0; i < inputSquare.GetLength(0); i++)
+			{
+				for (var j = 0; j < inputSquare.GetLength(1); j++)
+				{
+					result += Math.Abs(targetSquare[i, j] - inputSquare[i, j]);
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ KataTests/FormingMagicSquareTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using Kata;
+using Xunit;
+
+namespace KataTests
+{
+	public class FormingMagicSquareTest
+	{
+		private FormingMagicSquare _kata;
+
+		public FormingMagicSquareTest()
+		{
+			_kata = new FormingMagicSquare();
+		}
+
+		[Fact]
+		public void ValidMagicSquareCanPassValidator()
+		{
+			var validMagicSquareSequence = new[] { 4, 9, 2, 3, 5, 7, 8, 1, 6 };
+			var isMagicSquare = _kata.IsMagicSquare(_kata.ParseArrayToSquare(validMagicSquareSequence));
+
+			Assert.True(isMagicSquare);
+		}
+
+		[Fact]
+		public void InvalidMagicSquareCannotPassValidator()
+		{
+			var invalidMagicSquareSequence = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			var isMagicSquare = _kata.IsMagicSquare(_kata.ParseArrayToSquare(invalidMagicSquareSequence));
+
+			Assert.False(isMagicSquare);
+		}
+
+		[Fact]
+		public void SameMagicSquaresCostEqualsZero()
+		{
+			var targetSquare = _kata.ParseArrayToSquare( new[] { 4, 9, 2, 3, 5, 7, 8, 1, 6 });
+			var inputSquare = _kata.ParseArrayToSquare( new[] { 4, 9, 2, 3, 5, 7, 8, 1, 6 });
+
+			var expected = 0;
+			var actual = _kata.CalculateCost(inputSquare, targetSquare);
+			
+			Assert.Equal(expected,actual);
+		}
+
+		[Fact]
+		public void MagicSquaresOffByOneCostEqualsOne()
+		{
+			var targetSquare = _kata.ParseArrayToSquare( new[] { 4, 9, 2, 3, 5, 7, 8, 1, 6 });
+			var inputSquare = _kata.ParseArrayToSquare( new[] { 5, 9, 2, 3, 5, 7, 8, 1, 6 });
+
+			var expected = 1;
+			var actual = _kata.CalculateCost(inputSquare, targetSquare);
+			
+			Assert.Equal(expected,actual);
+		}
+	}
+}
