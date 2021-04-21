d0c6a28940a690ff65e1327a5f65b01e9a796893
Stephen Lin <stephen.sunglun.lin@gmail.com>
2021-04-22 00:26:31 +0800

    setup gitlab ci

---
+++ Kata/LeetCode/Medium/BestSightseeingPair.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace Kata.LeetCode.Medium
+{
+	public class BestSightseeingPair
+	{
+		public int MaxScoreSightseeingPair(int[] input)
+		{
+			var maxScore = 0;
+			var i = 0;
+			var j = 1;
+			while (true)
+			{
+				var score = Score(input, i, j);
+				if (score > maxScore)
+				{
+					Console.WriteLine($"i = {i}, j = {j}, A[i] + A[j] + i - j = {input[i]} + {input[j]} + {i} - {j} = {maxScore}");
+					maxScore = score;
+				}
+
+				j++;
+				if (j > input.Length-1)
+				{
+					i++;
+					j = i + 1;
+				}
+
+				if (i > input.Length-2)
+				{
+					return maxScore;
+				}
+			}
+			//for (var i = 0; i < input.Length-1; i++)
+			//{
+			//	for (var j = i + 1; j < input.Length; j++)
+			//	{
+			//		var score = Score(input, i, j);
+			//		if (score > maxScore)
+			//		{
+			//			maxScore = score;
+			//		}
+			//	}
+			//}
+
+			//return maxScore;
+		}
+
+		private int Score(int[] input, int indexI, int indexJ)
+		{
+			return input[indexI] + input[indexJ] + indexI - indexJ;
+		}
+	}
+}
---
+++ KataTests/LeetCode/Medium/BestSightseeingPairTest.cs
@@ -0,0 +1,24 @@
+﻿using Kata.LeetCode.Medium;
+using NUnit.Framework;
+
+namespace KataTests.LeetCode.Medium
+{
+	[TestFixture]
+	public class BestSightseeingPairTest
+	{
+		private BestSightseeingPair _kata;
+
+		[SetUp]
+		public void Setup()
+		{
+			_kata = new BestSightseeingPair();
+		}
+
+		[Test]
+		[TestCase(new[] { 8, 1, 5, 2, 6 }, ExpectedResult = 11)]
+		public int TestCases(int[] input)
+		{
+			return _kata.MaxScoreSightseeingPair(input);
+		}
+	}
+}
